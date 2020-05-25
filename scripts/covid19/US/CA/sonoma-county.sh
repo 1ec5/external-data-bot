04a0067b3a0d3c8d84834e6918d5daf8fb4c5c68
Minh Nguyễn <mxn@1ec5.org>
2020-05-24 17:50:51 -0700

    Sonoma County, California

---
+++ scripts/covid19/US/CA/sonoma-county.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Fetch the current table as JSON
+curl 'https://commons.wikimedia.org/wiki/Data:COVID-19_cases_in_Sonoma_County,_California.tab?action=raw' > commons.json
+
+# Query the FeatureServer table for cases by day
+curl 'https://services1.arcgis.com/P5Mv5GY5S66M8Z1Q/ArcGIS/rest/services/NCOV_Cases_Sonoma_County/FeatureServer/0/query?where=Date+<>+null&outFields=Date%2CNewCases%2CActive%2CDeaths%2CRecovered%2CCumulative&orderByFields=Date&f=json' > table.json
+
+# Convert date from number of milliseconds to YYYY-MM-DD
+# Update Commons
+jq -s --tab '.[1].data = [.[0].features[].attributes | [(.Date / 1000 | strftime("%Y-%m-%d")), .NewCases, .Active, .Deaths, .Recovered, .Cumulative]] | .[1]' table.json commons.json | expand -t4
