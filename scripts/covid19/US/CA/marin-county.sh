378ed4e158097af4ae6424d71003784b1651c42e
Minh Nguyễn <mxn@1ec5.org>
2020-05-25 22:22:24 -0700

    Marin County, California

---
+++ scripts/covid19/US/CA/marin-county.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Fetch the current table as JSON
+curl 'https://commons.wikimedia.org/wiki/Data:COVID-19_cases_in_Marin_County,_California.tab?action=raw' > commons.json
+
+# Query the FeatureServer table
+curl 'https://services6.arcgis.com/T8eS7sop5hLmgRRH/ArcGIS/rest/services/Covid19_Cumulative/FeatureServer/0/query?where=Date+<>+null+or+Date+%3D+null&outFields=Date%2CTotal_Cases%2CTotal_Recovered_%2CTotal_Hospitalized%2CTotal_Deaths&orderByFields=Date&f=json' > table.json
+
+# Convert date from number of milliseconds to YYYY-MM-DD
+jq '.features | map(.attributes | .Date = (.Date / 1000 | strftime("%Y-%m-%d")) | [.Date, .Total_Cases, .Total_Recovered_, .Total_Hospitalized, .Total_Deaths])' table.json > casesbyday.json
+
+# Update Commons
+jq -s --tab '.[1].data = .[0] | .[1]' casesbyday.json commons.json | expand -t4
