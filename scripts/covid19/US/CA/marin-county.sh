cf6178a8f98eb47b99795b3cbcdafcb6cc7a7add
Minh Nguyễn <mxn@1ec5.org>
2020-06-01 15:14:29 -0700

    Filter out empty Marin County entries

--- scripts/covid19/US/CA/marin-county.sh
+++ scripts/covid19/US/CA/marin-county.sh
@@ -8,7 +8,7 @@ curl 'https://services6.arcgis.com/T8eS7sop5hLmgRRH/ArcGIS/rest/services/Covid19
 
 # Filter out totally empty days
 # Convert date from number of milliseconds to YYYY-MM-DD
-jq '.features | map(.attributes | .Date = (.Date / 1000 | strftime("%Y-%m-%d")) | [.Date, (.Total_Cases | (strings // numbers) | tonumber) // null, (.Total_Recovered_ | (strings // numbers) | tonumber) // null, (.Total_Hospitalized | (strings // numbers) | tonumber) // null, (.Total_Deaths | (strings // numbers) | tonumber) // null])' table.json > casesbyday.json
+jq '.features | map(.attributes | select(.Total_Cases // .Total_Recovered_ // .Total_Hospitalized // .Total_Deaths) | .Date = (.Date / 1000 | strftime("%Y-%m-%d")) | [.Date, (.Total_Cases | (strings // numbers) | tonumber) // null, (.Total_Recovered_ | (strings // numbers) | tonumber) // null, (.Total_Hospitalized | (strings // numbers) | tonumber) // null, (.Total_Deaths | (strings // numbers) | tonumber) // null])' table.json > casesbyday.json
 
 # Update Commons
 jq -s --tab '.[1].data = .[0] | .[1]' casesbyday.json commons.json | expand -t4
