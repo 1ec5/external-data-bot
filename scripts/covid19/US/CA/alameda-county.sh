f751297a67464dcca7eb072093ba00e066b752c8
Minh Nguyễn <mxn@1ec5.org>
2020-07-05 08:49:14 -0700

    Switched Alameda County to new FeatureServer

--- scripts/covid19/US/CA/alameda-county.sh
+++ scripts/covid19/US/CA/alameda-county.sh
@@ -4,14 +4,14 @@
 curl 'https://commons.wikimedia.org/wiki/Data:COVID-19_cases_in_Alameda_County,_California.tab?action=raw' > commons.json
 
 # Fetch cases and deaths by day from the API
-curl 'https://services3.arcgis.com/1iDJcsklY3l3KIjE/arcgis/rest/services/AC_dates/FeatureServer/0/query?where=1%3D1&outFields=Date,BkLHJ_Cases,BkLHJ_CumulCases,BkLHJ_Deaths,BkLHJ_CumulDeaths,ACLHJ_Cases,ACLHJ_CumulCases,ACLHJ_Deaths,ACLHJ_CumulDeaths,AC_Cases,AC_CumulCases,AC_Deaths,AC_CumulDeaths&orderByFields=Date&f=json' > dates.json
+curl 'https://services3.arcgis.com/1iDJcsklY3l3KIjE/arcgis/rest/services/AC_dates2/FeatureServer/0/query?where=1%3D1&outFields=Date,BkLHJ_NewCases,BkLHJ_CumulCases,BkLHJ_NewDeaths,BkLHJ_CumulDeaths,ACLHJ_Cases,ACLHJ_CumulCases,ACLHJ_Deaths,ACLHJ_CumulDeaths,AC_Cases,AC_CumulCases,AC_Deaths,AC_CumulDeaths&orderByFields=Date&f=json' > dates.json
 
 # Fetch hospitalizations by day from the API
 curl 'https://services3.arcgis.com/1iDJcsklY3l3KIjE/arcgis/rest/services/AC_hospitalized2/FeatureServer/0/query?where=1%3D1&outFields=Date,Hospitalized_COVID_19_Positive_&outSR=4326&f=json' > hosp.json
 
 # Convert date from number of milliseconds to YYYY-MM-DD
 # Join hospitalizations to cases and deaths
-jq -s '[.[].features[].attributes | .Date = (.Date / 1000 | strftime("%Y-%m-%d"))] | group_by(.Date) | map(reduce .[] as $item ({}; . + $item) | [.Date, .BkLHJ_Cases, .BkLHJ_CumulCases, .BkLHJ_Deaths, .BkLHJ_CumulDeaths, .ACLHJ_Cases, .ACLHJ_CumulCases, .ACLHJ_Deaths, .ACLHJ_CumulDeaths, .AC_Cases, .AC_CumulCases, .AC_Deaths, .AC_CumulDeaths, .Hospitalized_COVID_19_Positive_])' dates.json hosp.json > data.json
+jq -s '[.[].features[].attributes | .Date = (.Date / 1000 | strftime("%Y-%m-%d"))] | group_by(.Date) | map(reduce .[] as $item ({}; . + $item) | [.Date, .BkLHJ_NewCases, .BkLHJ_CumulCases, .BkLHJ_NewDeaths, .BkLHJ_CumulDeaths, .ACLHJ_Cases, .ACLHJ_CumulCases, .ACLHJ_Deaths, .ACLHJ_CumulDeaths, .AC_Cases, .AC_CumulCases, .AC_Deaths, .AC_CumulDeaths, .Hospitalized_COVID_19_Positive_])' dates.json hosp.json > data.json
 
 # Update the table's existing entries with new data from the API
 jq -s --tab '.[0].data = .[1] | .[0]' commons.json data.json | expand -t4
