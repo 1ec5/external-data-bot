d2ac3a123bbab654f040856554225682753d037d
Minh Nguyễn <mxn@1ec5.org>
2020-07-05 08:42:58 -0700

    Fixed Alameda County when only hospitalizations have been updated

--- scripts/covid19/US/CA/alameda-county.sh
+++ scripts/covid19/US/CA/alameda-county.sh
@@ -10,8 +10,8 @@ curl 'https://services3.arcgis.com/1iDJcsklY3l3KIjE/arcgis/rest/services/AC_date
 curl 'https://services3.arcgis.com/1iDJcsklY3l3KIjE/arcgis/rest/services/AC_hospitalized2/FeatureServer/0/query?where=1%3D1&outFields=Date,Hospitalized_COVID_19_Positive_&outSR=4326&f=json' > hosp.json
 
 # Convert date from number of milliseconds to YYYY-MM-DD
-# Left join hospitalizations to cases and deaths
-jq -s '[[.[].features[].attributes | .Date = (.Date / 1000 | strftime("%Y-%m-%d"))] | reduce .[] as $item ({}; .[$item.Date] += $item) | to_entries[].value | .Hospitalized_COVID_19_Positive_ //= null | to_entries | map(.value)]' dates.json hosp.json > data.json
+# Join hospitalizations to cases and deaths
+jq -s '[.[].features[].attributes | .Date = (.Date / 1000 | strftime("%Y-%m-%d"))] | group_by(.Date) | map(reduce .[] as $item ({}; . + $item) | [.Date, .BkLHJ_Cases, .BkLHJ_CumulCases, .BkLHJ_Deaths, .BkLHJ_CumulDeaths, .ACLHJ_Cases, .ACLHJ_CumulCases, .ACLHJ_Deaths, .ACLHJ_CumulDeaths, .AC_Cases, .AC_CumulCases, .AC_Deaths, .AC_CumulDeaths, .Hospitalized_COVID_19_Positive_])' dates.json hosp.json > data.json
 
 # Update the table's existing entries with new data from the API
 jq -s --tab '.[0].data = .[1] | .[0]' commons.json data.json | expand -t4
