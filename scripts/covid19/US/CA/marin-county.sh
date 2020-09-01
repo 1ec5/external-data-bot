19853b2e4e4858d582067142fb34b3fc2405b087
Minh Nguyễn <mxn@1ec5.org>
2020-08-31 23:09:02 -0700

    Fetch Marin County from DataWrapper chart
    Rewrote the Marin County scraper to use the DataWrapper chart directly instead of the FeatureServer, which is no longer updated.

--- scripts/covid19/US/CA/marin-county.sh
+++ scripts/covid19/US/CA/marin-county.sh
@@ -3,12 +3,10 @@
 # Fetch the current table as JSON
 curl 'https://commons.wikimedia.org/wiki/Data:COVID-19_cases_in_Marin_County,_California.tab?action=raw' > commons.json
 
-# Query the FeatureServer table
-curl 'https://services6.arcgis.com/T8eS7sop5hLmgRRH/ArcGIS/rest/services/Covid19_Cumulative/FeatureServer/0/query?where=Date+<>+null+or+Date+%3D+null&outFields=Date%2CTotal_Cases%2CTotal_Recovered_%2CTotal_Hospitalized%2CTotal_Deaths&orderByFields=Date&f=json' > table.json
-
-# Filter out totally empty days
-# Convert date from number of milliseconds to YYYY-MM-DD
-jq '.features | map(.attributes | select(.Total_Cases // .Total_Recovered_ // .Total_Hospitalized // .Total_Deaths) | .Date = (.Date / 1000 | strftime("%Y-%m-%d")) | [.Date, (.Total_Cases | (strings // numbers) | tonumber) // null, (.Total_Recovered_ | (strings // numbers) | tonumber) // null, (.Total_Hospitalized | (strings // numbers) | tonumber) // null, (.Total_Deaths | (strings // numbers) | tonumber) // null])' table.json > casesbyday.json
+# Fetch the DataWrapper chart "Total Cases, Recovered, Hospitalizations and Deaths by Date Reported among Marin County Community Residents"
+# Isolate the CSV data in the JSON in the page script
+# Convert the CSV data to JSON
+curl 'https://datawrapper.dwcdn.net/Eq6Es/127/' | grep 'JSON.parse' | sed -E 's/.*JSON\.parse\((".+")\);.*/\1/' | jq 'fromjson.data.chartData | split("\r\n") | map(split(","))[1:] | map([(.[0] | strptime("%m/%d/%Y") | strftime("%Y-%m-%d"))] + (.[1:] | map(tonumber)))' > casesbyday.json
 
 # Update Commons
 jq -s --tab '.[1].data = .[0] | .[1]' casesbyday.json commons.json | expand -t4
