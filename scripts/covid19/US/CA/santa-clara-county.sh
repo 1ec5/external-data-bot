c672ce217028fb56746ab439e50e9b11362ad8d2
Minh Nguyễn <mxn@1ec5.org>
2020-11-03 00:29:38 -0800

    Corrected Santa Clara Co. timezone

--- scripts/covid19/US/CA/santa-clara-county.sh
+++ scripts/covid19/US/CA/santa-clara-county.sh
@@ -9,7 +9,7 @@ curl 'https://data.sccgov.org/resource/6cnm-gchg.json' | jq 'map({date: (.date |
 # Fetch the current cases by gender
 # Find the current total number of cases, including undated cases, by summing all the groups including Unknown
 # Total case count for today is the total case count as of today minus the undated case count
-curl 'https://data.sccgov.org/resource/ibdk-7rf5.json?$select=*,:updated_at' | jq '{date: (.[0][":updated_at"] | split("T")[0]), totalConfirmedCasesInclUndated: (map(.count | tonumber) | add)}' > today.json
+curl 'https://data.sccgov.org/resource/ibdk-7rf5.json?$select=*,:updated_at' | jq '{date: (.[0][":updated_at"][:19] + "Z" | fromdate | gmtime | .[3] -= 7 | mktime | strftime("%Y-%m-%d")), totalConfirmedCasesInclUndated: (map(.count | tonumber) | add)}' > today.json
 
 # Fetch the hospitalizations by day
 curl 'https://data.sccgov.org/resource/5xkz-6esm.json' | jq 'map({date: (.date | split("T")[0]), hospitalized: (.covid_total | tonumber), hospitalizedPUI: (.pui_total | tonumber)})' > hosp.json
