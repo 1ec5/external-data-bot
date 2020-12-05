a95ed41eb199b1447941ceb22f19759169f9540e
Minh Nguyễn <mxn@1ec5.org>
2020-12-05 01:06:45 -0800

    Rewrote Napa County
    Get time series from individual records in demographics FeatureServer.

--- scripts/covid19/US/CA/napa-county.sh
+++ scripts/covid19/US/CA/napa-county.sh
@@ -3,11 +3,29 @@
 # Fetch the current table as JSON
 curl 'https://commons.wikimedia.org/wiki/Data:COVID-19_cases_in_Napa_County,_California.tab?action=raw' > commons.json
 
-# Query the FeatureServer table for case counts
-curl 'https://services1.arcgis.com/Ko5rxt00spOfjMqj/ArcGIS/rest/services/Napa_Case_Regions/FeatureServer/0/query?where=1%3D1&returnGeometry=false&outStatistics=%5B%7B%22statisticType%22%3A%22max%22%2C%22onStatisticField%22%3A%22EditDate%22%2C%22outStatisticFieldName%22%3A%22date%22%7D%2C%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22IncidentCount%22%2C%22outStatisticFieldName%22%3A%22cases%22%7D%2C%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Death%22%2C%22outStatisticFieldName%22%3A%22deaths%22%7D%2C%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22RecoveredCount%22%2C%22outStatisticFieldName%22%3A%22recovered%22%7D%5D&f=json' > table.json
+# Query the demographics FeatureServer table for case collection dates
+curl 'https://services1.arcgis.com/Ko5rxt00spOfjMqj/ArcGIS/rest/services/CaseDataDemographics/FeatureServer/0/query?where=DtLabCollect<>NULL&outFields=DtLabCollect%2CCOUNT%28*%29+AS+newCases&returnDistinctValues=true&orderByFields=DtLabCollect&groupByFieldsForStatistics=DtLabCollect&f=json' > table.json
 
 # Convert date from number of milliseconds to YYYY-MM-DD
-jq '.features[].attributes | .date = (.date / 1000 | localtime | strftime("%Y-%m-%d"))' table.json > cases.json
+# Accumulate cases
+jq '.features | map(.attributes | .date = (.DtLabCollect / 1000 | localtime | strftime("%Y-%m-%d"))) | [foreach .[] as $date (0; . + $date.newCases; . as $cases | $date | .cases = $cases)]' table.json > cases.json
+
+# Query the demographics FeatureServer table for case result dates in cases where collection date is unknown
+curl 'https://services1.arcgis.com/Ko5rxt00spOfjMqj/ArcGIS/rest/services/CaseDataDemographics/FeatureServer/0/query?where=DtLabCollect%3DNULL&outFields=DtLabResult%2CCOUNT%28*%29+AS+newCases&returnDistinctValues=true&orderByFields=DtLabResult&groupByFieldsForStatistics=DtLabResult&f=json' > table.json
+
+# Convert date from number of milliseconds to YYYY-MM-DD
+# Accumulate cases
+jq '.features | map(.attributes | .date = (.DtLabResult / 1000 | localtime | strftime("%Y-%m-%d"))) | [foreach .[] as $date (0; . + $date.newCases; . as $cases | $date | .undatedCases = $cases)]' table.json > undatedcases.json
+
+# Query the demographics FeatureServer table for death dates
+curl 'https://services1.arcgis.com/Ko5rxt00spOfjMqj/ArcGIS/rest/services/CaseDataDemographics/FeatureServer/0/query?where=DtDeath<>NULL&outFields=DtDeath&f=json' > table.json
+
+# Convert date from number of milliseconds to YYYY-MM-DD
+# Accumulate deaths
+jq '.features | map(.attributes) | group_by(.DtDeath) | map({date: (.[0].DtDeath / 1000 | localtime | strftime("%Y-%m-%d")), newDeaths: length}) | [foreach .[] as $date (0; . + $date.newDeaths; . as $deaths | $date | .deaths = $deaths)]' table.json > deaths.json
+
+# Query the demographics FeatureServer table for recoveries
+RECOV=$(curl 'https://services1.arcgis.com/Ko5rxt00spOfjMqj/ArcGIS/rest/services/CaseDataDemographics/FeatureServer/0/query?where=Recovered='"'"'Y'"'"'&returnCountOnly=true&f=pjson' | jq '.count')
 
 # Fetch cumulative hospitalization counts from a LiveStories dashboard hooked up to a Google Sheets spreadsheet
 HOSP=$(curl 'https://legacy.livestories.com/dataset.json?dashId=5ec97d92a789540013c3298d' | jq '.series[].data[.categories | index("Cumulative/Acumulado")].y')
@@ -18,7 +36,7 @@ HOSP=$(curl 'https://legacy.livestories.com/dataset.json?dashId=5ec97d92a7895400
 LATEST_DATE=$(curl 'https://www.countyofnapa.org/2770/Situation-Update-Archive' | grep -oE '<li.*?> *(</?strong> *)+(\w+ +\d+ *, +\d+)' | grep -oE '\w+ +\d+ *, +\d+' | head -n 1)
 LATEST_DATE=$(date -jf '%b %d, %Y' "${LATEST_DATE}" '+%Y-%m-%d')
 
-jq ".date = \"${LATEST_DATE}\" | .hospitalized = ${HOSP}" cases.json > today.json
+echo '{}' | jq "{date: \"${LATEST_DATE}\", hospitalized: ${HOSP}, recovered: ${RECOV}}" > today.json
 
 # Update Commons
-jq -s --tab '.[0] as $today | .[1] | .data = (.data | map([["date", "cases", "recovered", "hospitalized", "deaths"], .] | transpose | map({key: .[0], value: .[1]}) | from_entries) + [$today] | group_by(.date) | map(add) | map([.date, .cases, .recovered, .hospitalized, .deaths]))' today.json commons.json | expand -t4
+jq -s --tab 'def eval_repeats(key): foreach .[] as $row (0; ($row[key] // .); . as $x | $row | (.[key] = (.[key] // $x))); .[0] as $commons | (.[0].data | map([["date", "cases-overwrite", "recovered", "hospitalized", "deaths-overwrite"], .] | transpose | map({key: .[0], value: .[1]}) | from_entries)) + .[1] + .[2] + .[3] + [.[4]] | group_by(.date) | map(add) | [eval_repeats("cases")] | [eval_repeats("undatedCases")] | [eval_repeats("deaths")] | map([.date, .cases + .undatedCases, .recovered, .hospitalized, .deaths]) as $data | $commons | .data = $data' commons.json cases.json undatedcases.json deaths.json today.json | expand -t4
