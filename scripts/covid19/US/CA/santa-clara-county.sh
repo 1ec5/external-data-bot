f7de2024485830e78551db0e1e8b478ca987d7ec
Minh Nguyễn <mxn@1ec5.org>
2021-01-27 23:33:14 -0800

    Fixed another bogus date in Santa Clara Co.

--- scripts/covid19/US/CA/santa-clara-county.sh
+++ scripts/covid19/US/CA/santa-clara-county.sh
@@ -15,7 +15,7 @@ curl 'https://data.sccgov.org/resource/ibdk-7rf5.json?$select=*,:updated_at' | j
 curl 'https://data.sccgov.org/resource/5xkz-6esm.json' | jq 'map(.date = (.date | split("T")[0]) | {date: (if .date >= "2021-12-27" then (.date | sub("^2021"; "2020")) else .date end), hospitalized: (.covid_total | tonumber), hospitalizedPUI: (.pui_total | tonumber)})' > hosp.json
 
 # Fetch the deaths by day
-curl 'https://data.sccgov.org/resource/tg4j-23y2.json' | jq 'map(.date = (.date | split("T")[0]) | {date: (.date | sub("^2012"; "2021") | sub("^1921-01-19"; "2021-01-19")), deaths: (.cumulative | tonumber)})' > deaths.json
+curl 'https://data.sccgov.org/resource/tg4j-23y2.json' | jq 'map(.date = (.date | split("T")[0]) | {date: (.date | sub("^2012"; "2021") | sub("^1944-01-18"; "2021-01-18")), deaths: (.cumulative | tonumber)})' > deaths.json
 
 # Update the table's existing entries with new data from the dashboard, adding a row for today
 # New case count for today is total case count for today minus total case count for yesterday
