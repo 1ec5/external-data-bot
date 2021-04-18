7d9c33668c48ef19f4fd842833278c99dab9861f
Minh Nguyễn <mxn@1ec5.org>
2021-04-17 22:44:19 -0700

    Increased SODA API limit for San Francisco

--- scripts/covid19/US/CA/san-francisco.sh
+++ scripts/covid19/US/CA/san-francisco.sh
@@ -4,7 +4,7 @@
 curl 'https://commons.wikimedia.org/wiki/Data:COVID-19_cases_in_San_Francisco.tab?action=raw' > commons.json
 
 # Fetch the new cases and deaths by day from the API
-curl https://data.sfgov.org/resource/tvq9-ec9w.json > tvq9-ec9w.json
+curl 'https://data.sfgov.org/resource/tvq9-ec9w.json?$limit=2000' > tvq9-ec9w.json
 
 # Convert date from full timestamp to YYYY-MM-DD
 # Pivot on dates as rows and counts by case disposition as columns
@@ -12,7 +12,7 @@ curl https://data.sfgov.org/resource/tvq9-ec9w.json > tvq9-ec9w.json
 jq -s '.[0] | group_by(.specimen_collection_date)[] | {date: (.[0].specimen_collection_date | strptime("%Y-%m-%dT%H:%M:%S.000") | strftime("%Y-%m-%d")), newConfirmedCases: (reduce (.[] | select(.case_disposition == "Confirmed").case_count | tonumber) as $count (0; . + $count)), newDeaths: (reduce (.[] | select(.case_disposition == "Death").case_count | tonumber) as $count (0; . + $count))}' tvq9-ec9w.json | jq -s 'foreach .[] as $row (0; . + $row.newConfirmedCases; . as $x | $row | (.totalConfirmedCases = $x))' | jq -s 'foreach .[] as $row (0; . + $row.newDeaths; . as $x | $row | (.totalDeaths = $x))' | jq -s > cases.json
 
 # Fetch hospitalizations by day from the API
-curl https://data.sfgov.org/resource/nxjg-bhem.json > nxjg-bhem.json
+curl 'https://data.sfgov.org/resource/nxjg-bhem.json?$limit=2500' > nxjg-bhem.json
 
 # Convert date from full timestamp to YYYY-MM-DD
 # Pivot on dates as rows and counts as columns
