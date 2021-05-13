ecd2637846b9314abad0d4a7090fea9c27ae5ad2
Minh Nguyễn <mxn@1ec5.org>
2021-05-12 23:16:51 -0700

    Worked around missing Sonoma County new daily cases data

--- scripts/covid19/US/CA/sonoma-county.sh
+++ scripts/covid19/US/CA/sonoma-county.sh
@@ -8,4 +8,4 @@ curl 'https://services1.arcgis.com/P5Mv5GY5S66M8Z1Q/ArcGIS/rest/services/NCOV_Ca
 
 # Convert date from number of milliseconds to YYYY-MM-DD
 # Update Commons
-jq -s --tab '.[1].data = ([.[0].features[].attributes | (.Date / 1000 | gmtime | .[3] -= 7 | mktime | strftime("%Y-%m-%d")) as $date | [$date, .NewCases, .Active, .Deaths, .Recovered, .Cumulative]] | sort_by(.[0])) | .[1]' table.json commons.json | expand -t4
+jq -s --tab 'def diff(key): [[{(key): 0}] + .[:-1], .] | transpose | map(.[1]["diff:" + key] = (if .[1][key] and .[0][key] then (.[1][key] - .[0][key]) else null end) | .[1]); .[1].data = ([.[0].features[].attributes | (.Date / 1000 | gmtime | .[3] -= 7 | mktime | strftime("%Y-%m-%d")) as $date | [$date, .NewCases, .Active, .Deaths, .Recovered, .Cumulative]] | sort_by(.[0]) | map({cumulative: .[5], row: .}) | diff("cumulative") | map(.row[1] = (.row[1] // .["diff:cumulative"]) | .row)) | .[1]' table.json commons.json | expand -t4
