1ec5/external-data-bot

commit c882291c3d7048733d4f4e9dfb35caf505cba3f0

Fixed bogus date in Santa Clara Co.

scripts/covid19/US/CA/santa-clara-county.sh
@@ -15,7 +15,7 @@ curl 'https://data.sccgov.org/resource/ibdk-7rf5.json?$select=*,:updated_at' | j
 curl 'https://data.sccgov.org/resource/5xkz-6esm.json' | jq 'map(.date = (.date | split("T")[0]) | {date: (if .date >= "2021-12-27" then (.date | sub("^2021"; "2020")) else .date end), hospitalized: (.covid_total | tonumber), hospitalizedPUI: (.pui_total | tonumber)})' > hosp.json
 
 # Fetch the deaths by day
-curl 'https://data.sccgov.org/resource/tg4j-23y2.json' | jq 'def extrapolate: foreach .[] as $row (0; ($row.date // .); . as $x | $row | .date = (.date // ($x | .[:-4] + "Z" | fromdate | gmtime | .[2] += 1 | mktime | todate))); [extrapolate] | map(.date = (.date | split("T")[0]) | {date: (.date | sub("^2012"; "2021") | sub("^1944-01-18"; "2021-01-18")), deaths: (.cumulative | tonumber)})' > deaths.json
+curl 'https://data.sccgov.org/resource/tg4j-23y2.json' | jq 'def extrapolate: foreach .[] as $row (0; ($row.date // .); . as $x | $row | .date = (.date // ($x | .[:-4] + "Z" | fromdate | gmtime | .[2] += 1 | mktime | todate))); [extrapolate] | map(.date = (.date | split("T")[0]) | {date: (.date | sub("^2021-12-21"; "2020-12-21")), deaths: (if .date = "2021-12-21" then null else (.cumulative | tonumber) end)})' > deaths.json
 
 # Update the table's existing entries with new data from the dashboard, adding a row for today
 # New case count for today is total case count for today minus total case count for yesterday

commit 06b5b487e39e9cbcf5da13950fda05dd6de31a53

Fixed deaths in Santa Clara Co.
A missing = in c882291c3d7048733d4f4e9dfb35caf505cba3f0 caused cumulative deaths in Santa Clara County to always be zero.

scripts/covid19/US/CA/santa-clara-county.sh
@@ -20,7 +20,7 @@ curl 'https://data.sccgov.org/resource/5xkz-6esm.json' | jq 'map(.date = (.date 
 #jq 'def propagate_repeats(dm1): if .R == 2 then .C = .C[:1] + [null] + .C[1:] elif .R == 4 then .C = .C[:2] + [null] + .C[2:] elif .R == 6 then .C = .C[:1] + [null, null] + .C[1:] else . end; def eval_repeats(series): series | map(.C) | foreach .[] as $row ([]; [., $row] | transpose | map(.[1] // .[0]); .); .results | map(.result.data | (.descriptor.Select | map(.Name) | .[0] = "date") as $desc | .dsr.DS[0].PH[0].DM0 | map(select(has("Ø") | not) | propagate_repeats(.)) | [eval_repeats(.)] | map([$desc, (.[0] = (.[0] / 1000 | strftime("%Y-%m-%d")))] | transpose | map({(.[0]): .[1]}) | add)) | add | group_by(.date) | map(add | .hospitalized = .["Sum(trends.covid_total)"] | .hospitalizedPUI = .["Sum(trends.pui_total)"])' dashboard.json > hosp.json
 
 # Fetch the deaths by day
-curl 'https://data.sccgov.org/resource/tg4j-23y2.json' | jq 'def extrapolate: foreach .[] as $row (0; ($row.date // .); . as $x | $row | .date = (.date // ($x | .[:-4] + "Z" | fromdate | gmtime | .[2] += 1 | mktime | todate))); [extrapolate] | map(.date = (.date | split("T")[0]) | {date: (.date | sub("^1927"; "2021")), deaths: (if .date = "2021-12-21" then null else (.cumulative | tonumber) end)})' > deaths.json
+curl 'https://data.sccgov.org/resource/tg4j-23y2.json' | jq 'def extrapolate: foreach .[] as $row (0; ($row.date // .); . as $x | $row | .date = (.date // ($x | .[:-4] + "Z" | fromdate | gmtime | .[2] += 1 | mktime | todate))); [extrapolate] | map(.date = (.date | split("T")[0]) | {date: (.date | sub("^1927"; "2021")), deaths: (.cumulative | tonumber)})' > deaths.json
 
 # Update the table's existing entries with new data from the dashboard, adding a row for today
 # New case count for today is total case count for today minus total case count for yesterday