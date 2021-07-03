ad21da25751fad4393e14f47f8c655ca7aca61bc
Minh Nguyễn <mxn@1ec5.org>
2021-07-03 01:02:38 -0700

    Updated Contra Costa Co. QlikSense calls

--- scripts/covid19/US/CA/contra-costa-county.sh
+++ scripts/covid19/US/CA/contra-costa-county.sh
@@ -7,21 +7,21 @@ curl 'https://commons.wikimedia.org/wiki/Data:COVID-19_cases_in_Contra_Costa_Cou
 (
     sleep 2; echo '{"delta":true,"handle":-1,"method":"OpenDoc","params":["b7d7f869-fb91-4950-9262-0b89473ceed6","","","",false],"id":1,"jsonrpc":"2.0"}'
     sleep 2; echo '{"delta":true,"handle":1,"method":"GetObject","params":["cWjnGdK"],"id":3,"jsonrpc":"2.0"}'
-    sleep 2; echo '{"delta":true,"handle":2,"method":"GetLayout","params":[],"id":5,"jsonrpc":"2.0"}'
+    sleep 2; echo '{"delta":true,"handle":2,"method":"GetHyperCubeContinuousData","params":["/qHyperCubeDef",{"qStart":42000,"qEnd":50000,"qNbrPoints":2000},false],"id":115,"jsonrpc":"2.0"}'
     sleep 4
-) | websocat -B 131071 'wss://dashboard.cchealth.org/app/b7d7f869-fb91-4950-9262-0b89473ceed6' | tail -n 1 > dashboard.json
+) | websocat -B 135000 'wss://dashboard.cchealth.org/app/b7d7f869-fb91-4950-9262-0b89473ceed6' | tail -n 1 > dashboard.json
 # Convert date from number of days since 1899-12-30 to YYYY-MM-DD
-jq '.result.qLayout[].value.qHyperCube.qDataPages[].qMatrix | map({date: ((.[0].qNum - 2) * 24 * 60 * 60 | gmtime | .[0] -= 70 | strftime("%Y-%m-%d")), newCases: .[1].qNum})' dashboard.json > newcases.json
+jq '.result.qDataPages[].value[].qMatrix | map({date: ((.[0].qNum - 2) * 24 * 60 * 60 | gmtime | .[0] -= 70 | strftime("%Y-%m-%d")), newCases: .[1].qNum})' dashboard.json > newcases.json
 
 # Fetch the total cases by day from the dashboard over JSON-RPC
 (
-    sleep 1; echo '{"delta":true,"handle":-1,"method":"OpenDoc","params":["b7d7f869-fb91-4950-9262-0b89473ceed6","","","",false],"id":1,"jsonrpc":"2.0"}'
-    sleep 1; echo '{"delta":true,"handle":1,"method":"GetObject","params":["jWjFxe"],"id":3,"jsonrpc":"2.0"}'
-    sleep 1; echo '{"delta":true,"handle":2,"method":"GetLayout","params":[],"id":5,"jsonrpc":"2.0"}'
+    sleep 2; echo '{"delta":true,"handle":-1,"method":"OpenDoc","params":["b7d7f869-fb91-4950-9262-0b89473ceed6","","","",false],"id":1,"jsonrpc":"2.0"}'
+    sleep 2; echo '{"delta":true,"handle":1,"method":"GetObject","params":["jWjFxe"],"id":5,"jsonrpc":"2.0"}'
+    sleep 2; echo '{"delta":true,"handle":2,"method":"GetHyperCubeContinuousData","params":["/qHyperCubeDef",{"qStart":42000,"qEnd":50000,"qNbrPoints":2000},false],"id":115,"jsonrpc":"2.0"}'
     sleep 3
-) | websocat -B 131071 'wss://dashboard.cchealth.org/app/b7d7f869-fb91-4950-9262-0b89473ceed6' | tail -n 1 > dashboard.json
+) | websocat -B 135000 'wss://dashboard.cchealth.org/app/b7d7f869-fb91-4950-9262-0b89473ceed6' | tail -n 1 > dashboard.json
 # Convert date from MM/DD/YYYY to YYYY-MM-DD
-jq '.result.qLayout[].value.qHyperCube.qDataPages[].qMatrix | map({date: ((.[0].qNum - 2) * 24 * 60 * 60 | gmtime | .[0] -= 70 | strftime("%Y-%m-%d")), cases: .[1].qNum})' dashboard.json > totalcases.json
+jq '.result.qDataPages[].value[].qMatrix | map({date: ((.[0].qNum - 2) * 24 * 60 * 60 | gmtime | .[0] -= 70 | strftime("%Y-%m-%d")), cases: .[1].qNum})' dashboard.json > totalcases.json
 
 # Fetch the total recoveries from the dashboard over JSON-RPC
 (
@@ -29,18 +29,18 @@ jq '.result.qLayout[].value.qHyperCube.qDataPages[].qMatrix | map({date: ((.[0].
     sleep 2; echo '{"delta":true,"handle":1,"method":"GetObject","params":["LuVGBZ"],"id":3,"jsonrpc":"2.0"}'
     sleep 2; echo '{"delta":true,"handle":2,"method":"GetLayout","params":[],"id":5,"jsonrpc":"2.0"}'
     sleep 4
-) | websocat 'wss://dashboard.cchealth.org/app/b7d7f869-fb91-4950-9262-0b89473ceed6' | tail -n 1 > dashboard.json
+) | websocat -B 135000 'wss://dashboard.cchealth.org/app/b7d7f869-fb91-4950-9262-0b89473ceed6' | tail -n 1 > dashboard.json
 RECOV=$(jq '.result.qLayout[].value.qHyperCube.qDataPages[].qMatrix[][].qNum' dashboard.json)
 
 # Fetch the new deaths by day from the dashboard over JSON-RPC
 (
     sleep 2; echo '{"delta":true,"handle":-1,"method":"OpenDoc","params":["b7d7f869-fb91-4950-9262-0b89473ceed6","","","",false],"id":1,"jsonrpc":"2.0"}'
     sleep 2; echo '{"delta":true,"handle":1,"method":"GetObject","params":["zKvfuW"],"id":3,"jsonrpc":"2.0"}'
-    sleep 2; echo '{"delta":true,"handle":2,"method":"GetLayout","params":[],"id":5,"jsonrpc":"2.0"}'
+    sleep 2; echo '{"delta":true,"handle":2,"method":"GetHyperCubeContinuousData","params":["/qHyperCubeDef",{"qStart":42000,"qEnd":50000,"qNbrPoints":2000},false],"id":115,"jsonrpc":"2.0"}'
     sleep 4
-) | websocat -B 131071 'wss://dashboard.cchealth.org/app/b7d7f869-fb91-4950-9262-0b89473ceed6' | tail -n 1 > dashboard.json
+) | websocat -B 135000 'wss://dashboard.cchealth.org/app/b7d7f869-fb91-4950-9262-0b89473ceed6' | tail -n 1 > dashboard.json
 # Calculate a running total
-jq '[.result.qLayout[].value.qHyperCube.qDataPages[].qMatrix | map({date: ((.[0].qNum - 2) * 24 * 60 * 60 | gmtime | .[0] -= 70 | strftime("%Y-%m-%d")), newDeaths: (.[1].qNum + .[2].qNum)}) | foreach .[] as $row (0; . + $row.newDeaths; . as $x | $row | (.deaths = $x))]' dashboard.json > deaths.json
+jq '[.result.qDataPages[].value[].qMatrix | map({date: ((.[0].qNum - 2) * 24 * 60 * 60 | gmtime | .[0] -= 70 | strftime("%Y-%m-%d")), newDeaths: (.[1].qNum + .[2].qNum)}) | foreach .[] as $row (0; . + $row.newDeaths; . as $x | $row | (.deaths = $x))]' dashboard.json > deaths.json
 
 # Join the new and total cases and death tolls
 # Insert an entry for recoveries on the latest date
