b39ac7b0857cc4bea3387657705bf1172362091b
Minh Nguyễn <mxn@1ec5.org>
2021-07-03 12:17:52 -0700

    Rewrote Contra Costa Co.
    Fetch Contra Costa County time series from the original table over JSON-RPC to improve performance, close gaps in recovery numbers, and add hospitalizations.

--- scripts/covid19/US/CA/contra-costa-county.sh
+++ scripts/covid19/US/CA/contra-costa-county.sh
@@ -3,48 +3,25 @@
 # Fetch the current table as JSON
 curl 'https://commons.wikimedia.org/wiki/Data:COVID-19_cases_in_Contra_Costa_County,_California.tab?action=raw' > commons.json
 
-# Fetch the new cases by day from the dashboard over JSON-RPC
+# Fetch tables and keys from the dashboard over JSON-RPC
+# https://qlik.dev/apis/json-rpc/qix/doc#%23%2Fentries%2FDoc%2Fentries%2FGetTablesAndKeys
+#(
+#    sleep 2; echo '{"delta":true,"handle":-1,"method":"OpenDoc","params":["b7d7f869-fb91-4950-9262-0b89473ceed6","","","",false],"id":1,"jsonrpc":"2.0"}'
+#    sleep 2; echo '{"jsonrpc":"2.0","id":6,"handle":1,"method":"GetTablesAndKeys","params":{"qWindowSize":{"qcx":1,"qcy":1},"qNullSize":{"qcx":1,"qcy":1},"qCellHeight":1,"qSyntheticMode":true,"qIncludeSysVars":false,"qIncludeProfiling":false}}'
+#    sleep 4
+#) | websocat -B 1000000 'wss://dashboard.cchealth.org/app/b7d7f869-fb91-4950-9262-0b89473ceed6' | tail -n 1 > tables.json
+#jq '.result.qtr | map({name: .qName, fields: .qFields | map(.qName)})' tables.json
+
+# Fetch time series data from the dashboard over JSON-RPC
+# https://qlik.dev/apis/json-rpc/qix/doc#%23%2Fentries%2FDoc%2Fentries%2FGetTableData
 (
     sleep 2; echo '{"delta":true,"handle":-1,"method":"OpenDoc","params":["b7d7f869-fb91-4950-9262-0b89473ceed6","","","",false],"id":1,"jsonrpc":"2.0"}'
-    sleep 2; echo '{"delta":true,"handle":1,"method":"GetObject","params":["cWjnGdK"],"id":3,"jsonrpc":"2.0"}'
-    sleep 2; echo '{"delta":true,"handle":2,"method":"GetHyperCubeContinuousData","params":["/qHyperCubeDef",{"qStart":42000,"qEnd":50000,"qNbrPoints":2000},false],"id":115,"jsonrpc":"2.0"}'
+    sleep 2; echo '{"jsonrpc":"2.0","id":6,"handle":1,"method":"GetTableData","params":{"qOffset":0,"qRows":600,"qSyntheticMode":true,"qTableName":"NUMBERS_BY_DATE"}}'
     sleep 4
-) | websocat -B 135000 'wss://dashboard.cchealth.org/app/b7d7f869-fb91-4950-9262-0b89473ceed6' | tail -n 1 > dashboard.json
+) | websocat -B 1000000 'wss://dashboard.cchealth.org/app/b7d7f869-fb91-4950-9262-0b89473ceed6' | tail -n 1 > dashboard.json
 # Convert date from number of days since 1899-12-30 to YYYY-MM-DD
-jq '.result.qDataPages[].value[].qMatrix | map({date: ((.[0].qNum - 2) * 24 * 60 * 60 | gmtime | .[0] -= 70 | strftime("%Y-%m-%d")), newCases: .[1].qNum})' dashboard.json > newcases.json
+# Calculate running total of deaths
+jq 'def total(key): foreach .[] as $row (0; . + $row[key]; . as $x | $row | (.["total:" + key] = $x)); .result.qData | map(.qValue | {date: ((.[0].qNumber - 2) * 24 * 60 * 60 | gmtime | .[0] -= 70 | strftime("%Y-%m-%d")), newCases: .[20].qNumber, cases: .[22].qNumber, newDeaths: .[26].qNumber, recoveries: .[13].qNumber, hospitalized: .[6].qNumber}) | sort_by(.date) | [total("newDeaths")] | map(.deaths = .["total:newDeaths"])' dashboard.json > casesbyday.json
 
-# Fetch the total cases by day from the dashboard over JSON-RPC
-(
-    sleep 2; echo '{"delta":true,"handle":-1,"method":"OpenDoc","params":["b7d7f869-fb91-4950-9262-0b89473ceed6","","","",false],"id":1,"jsonrpc":"2.0"}'
-    sleep 2; echo '{"delta":true,"handle":1,"method":"GetObject","params":["jWjFxe"],"id":5,"jsonrpc":"2.0"}'
-    sleep 2; echo '{"delta":true,"handle":2,"method":"GetHyperCubeContinuousData","params":["/qHyperCubeDef",{"qStart":42000,"qEnd":50000,"qNbrPoints":2000},false],"id":115,"jsonrpc":"2.0"}'
-    sleep 3
-) | websocat -B 135000 'wss://dashboard.cchealth.org/app/b7d7f869-fb91-4950-9262-0b89473ceed6' | tail -n 1 > dashboard.json
-# Convert date from MM/DD/YYYY to YYYY-MM-DD
-jq '.result.qDataPages[].value[].qMatrix | map({date: ((.[0].qNum - 2) * 24 * 60 * 60 | gmtime | .[0] -= 70 | strftime("%Y-%m-%d")), cases: .[1].qNum})' dashboard.json > totalcases.json
-
-# Fetch the total recoveries from the dashboard over JSON-RPC
-(
-    sleep 2; echo '{"delta":true,"handle":-1,"method":"OpenDoc","params":["b7d7f869-fb91-4950-9262-0b89473ceed6","","","",false],"id":1,"jsonrpc":"2.0"}'
-    sleep 2; echo '{"delta":true,"handle":1,"method":"GetObject","params":["LuVGBZ"],"id":3,"jsonrpc":"2.0"}'
-    sleep 2; echo '{"delta":true,"handle":2,"method":"GetLayout","params":[],"id":5,"jsonrpc":"2.0"}'
-    sleep 4
-) | websocat -B 135000 'wss://dashboard.cchealth.org/app/b7d7f869-fb91-4950-9262-0b89473ceed6' | tail -n 1 > dashboard.json
-RECOV=$(jq '.result.qLayout[].value.qHyperCube.qDataPages[].qMatrix[][].qNum' dashboard.json)
-
-# Fetch the new deaths by day from the dashboard over JSON-RPC
-(
-    sleep 2; echo '{"delta":true,"handle":-1,"method":"OpenDoc","params":["b7d7f869-fb91-4950-9262-0b89473ceed6","","","",false],"id":1,"jsonrpc":"2.0"}'
-    sleep 2; echo '{"delta":true,"handle":1,"method":"GetObject","params":["zKvfuW"],"id":3,"jsonrpc":"2.0"}'
-    sleep 2; echo '{"delta":true,"handle":2,"method":"GetHyperCubeContinuousData","params":["/qHyperCubeDef",{"qStart":42000,"qEnd":50000,"qNbrPoints":2000},false],"id":115,"jsonrpc":"2.0"}'
-    sleep 4
-) | websocat -B 135000 'wss://dashboard.cchealth.org/app/b7d7f869-fb91-4950-9262-0b89473ceed6' | tail -n 1 > dashboard.json
-# Calculate a running total
-jq '[.result.qDataPages[].value[].qMatrix | map({date: ((.[0].qNum - 2) * 24 * 60 * 60 | gmtime | .[0] -= 70 | strftime("%Y-%m-%d")), newDeaths: (.[1].qNum + .[2].qNum)}) | foreach .[] as $row (0; . + $row.newDeaths; . as $x | $row | (.deaths = $x))]' dashboard.json > deaths.json
-
-# Join the new and total cases and death tolls
-# Insert an entry for recoveries on the latest date
-# Backfill recoveries from Commons
 # Update Commons
-JQJOINRECOV=('.[3] as $commons | ($commons.data | INDEX(.[0])) as $oldData | .[:3] | transpose | map(reduce .[] as $item ({}; . + $item)) | .[-1].recoveries =' ${RECOV} '| map([.date, .newCases, .cases, .newDeaths, .deaths, .recoveries // $oldData[.date][5]]) as $newData | $commons | .data = $newData')
-jq -s --tab "${JQJOINRECOV[*]}" newcases.json totalcases.json deaths.json commons.json | expand -t4
+jq -s --tab '.[1].data = (.[0] | map([.date, .newCases, .cases, .newDeaths, .deaths, .recoveries, .hospitalized])) | .[1]' casesbyday.json commons.json | expand -t4
