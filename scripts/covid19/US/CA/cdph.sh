0578e4c1e1259707a2c53a19a129b211f01ce43f
Minh Nguyễn <mxn@1ec5.org>
2022-02-21 03:40:27 -0800

    Added California Dept. of Public Health stats by county

---
+++ scripts/covid19/US/CA/cdph.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Fetch the current table as JSON
+curl 'https://commons.wikimedia.org/wiki/Data:COVID-19_cases_in_California_by_county.tab?action=raw' > commons.json
+
+# Fetch the latest day's statistics by county from CKAN
+# SELECT * FROM (SELECT date,area,population,cumulative_total_tests,cumulative_positive_tests,cumulative_cases,cumulative_deaths,RANK() OVER (PARTITION BY area ORDER BY date DESC) area_rank FROM "046cdd2b-31e5-4d34-9ed3-b48cdbc4be7a" WHERE date IS NOT NULL) p WHERE p.area_rank = 1
+curl 'https://data.chhs.ca.gov/api/3/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%28SELECT%20date,area,population,cumulative_total_tests,cumulative_positive_tests,cumulative_cases,cumulative_deaths,RANK%28%29%20OVER%20%28PARTITION%20BY%20area%20ORDER%20BY%20date%20DESC%29%20area_rank%20FROM%20%22046cdd2b-31e5-4d34-9ed3-b48cdbc4be7a%22%20WHERE%20date%20IS%20NOT%20NULL%29%20p%20WHERE%20p.area_rank%20=%201' > dashboard.json
+
+# Update Commons
+jq -s --tab '.[0].data = (.[1].result.records | map([.date, .area, (.population | values | tonumber) // null, (.cumulative_total_tests | values | tonumber) // null, (.cumulative_positive_tests | values | tonumber) // null, (.cumulative_cases | values | tonumber) // null, (.cumulative_deaths | values | tonumber) // null])) | .[0]' commons.json dashboard.json | expand -t4
