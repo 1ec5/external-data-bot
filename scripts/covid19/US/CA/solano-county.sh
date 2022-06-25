092d26f9ecfabc520da10f1c6c15cbf6a1c3327e
Minh Nguyễn <mxn@1ec5.org>
2022-06-25 11:59:03 -0700

    Rewrote Solano Co.
    Solano Co. moved to a different set of FeatureServers back in March. Attempted to maintain parity and consistency with that older set.

--- scripts/covid19/US/CA/solano-county.sh
+++ scripts/covid19/US/CA/solano-county.sh
@@ -3,13 +3,24 @@
 # Fetch the current table as JSON
 curl 'https://commons.wikimedia.org/wiki/Data:COVID-19_cases_in_Solano_County,_California.tab?action=raw' > commons.json
 
-# Query the FeatureServer table
-curl 'https://services2.arcgis.com/SCn6czzcqKAFwdGU/ArcGIS/rest/services/COVID19Surveypt1v3_view/FeatureServer/0/query?where=1%3D1&outFields=Date_reported%2Ccumulative_cases%2CActive_cases%2Ccurrently_hospitalized_cases%2Ctotal_hospitalizations%2Ctotal_deaths&returnGeometry=false&orderByFields=Date_reported&f=json' > table.json
+# Query the FeatureServer epidemiological curve table
+curl 'https://services2.arcgis.com/SCn6czzcqKAFwdGU/ArcGIS/rest/services/Epi_Curve/FeatureServer/0/query?where=1%3D1&outFields=date_collected%2Cdaily_number%2Ccumulative_running_total_number&orderByFields=date_collected&f=json' > table.json
 
 # Convert date from number of milliseconds to YYYY-MM-DD
+# Accumulate cases
 # Calculate recoveries by subtracting deaths and active cases from confirmed cases
-# Calculate new cases by day
-jq 'def eval_repeats(key): foreach .[] as $row (0; ($row[key] // .); . as $x | $row | (.[key] = (.[key] // $x))); def diff(key): [[{(key): 0}] + .[:-1], .] | transpose | map(.[1]["diff:" + key] = (if .[1][key] and .[0][key] then (.[1][key] - .[0][key]) else null end) | .[1]); .features | map(select(.attributes | .cumulative_cases or .currently_hospitalized_cases) | .attributes | {date: (.Date_reported / 1000 | gmtime | .[3] -= 7 | mktime | strftime("%Y-%m-%d")), active: .Active_cases, recovered: (try (.cumulative_cases - .total_deaths - .Active_cases) catch null), currentlyHospitalized: .currently_hospitalized_cases, totalHospitalized: .total_hospitalizations, deaths: .total_deaths, totalConfirmedCases: .cumulative_cases}) | .[-1] as $today | map(.repeated = (.date != $today.date and (.totalConfirmedCases == $today.totalConfirmedCases and .deaths == $today.deaths and .totalHospitalized == $today.totalHospitalized)) | .active = (if .repeated then null else .active end) | .recovered = (if .repeated then null else .recovered end) | .totalHospitalized = (if .repeated then null else .totalHospitalized end) | .deaths = (if .repeated then null else .deaths end) | .totalConfirmedCases = (if .repeated then null else .totalConfirmedCases end)) | [eval_repeats("active")] | [eval_repeats("recovered")] | [eval_repeats("totalHospitalized")] | [eval_repeats("deaths")] | [eval_repeats("totalConfirmedCases")] | diff("totalConfirmedCases") | map(.newCases = .["diff:totalConfirmedCases"])' table.json > casesbyday.json
+jq '.features | map(.attributes | {date: (.date_collected / 1000 | gmtime | .[3] -= 7 | mktime | strftime("%Y-%m-%d")), newCases: .daily_number, active: .cumulative_running_total_number}) | [foreach .[] as $date (0; . + $date.newCases; . as $cases | $date | .cases = $cases)]' table.json > casesbyday.json
+
+# Query the FeatureServer hospitalization table
+curl 'https://services2.arcgis.com/SCn6czzcqKAFwdGU/arcgis/rest/services/Hospital_Stats/FeatureServer/0/query?where=number_inpatient_by_day+IS+NOT+NULL&outFields=date_%2Cnumber_inpatient_by_day&orderByFields=date_&f=json' > table.json
+
+# Convert date from number of milliseconds to YYYY-MM-DD
+jq '.features | map(.attributes | {date: (.date_ / 1000 | gmtime | .[3] -= 7 | mktime | strftime("%Y-%m-%d")), hospitalized: .number_inpatient_by_day})' table.json > hosp.json
+
+# Query the FeatureServer vaccination table and its metadata
+curl 'https://services2.arcgis.com/SCn6czzcqKAFwdGU/ArcGIS/rest/services/Vaccine_Summary/FeatureServer/0/query?where=1%3D1&outFields=*&f=json' > table.json
+VAX_DATE=$(curl 'https://services2.arcgis.com/SCn6czzcqKAFwdGU/ArcGIS/rest/services/Vaccine_Summary/FeatureServer/0?f=json' | jq '.editingInfo.lastEditDate | . / 1000 | gmtime | .[3] -= 7 | mktime | strftime("%Y-%m-%d")')
+jq ".features[0].attributes | {date: ${VAX_DATE}, cases: .cumulative_cases, active: .active_cases, totalHospitalized: .total_hospitalizations, deaths: .total_deaths}" table.json > today.json
 
 # Update Commons
-jq -s --tab '.[1].data = (((.[1].data | map({date: .[0], newCases: .[1], active: .[2], recovered: .[3], currentlyHospitalized: .[4], totalHospitalized: .[5], deaths: .[6], totalConfirmedCases: .[7]})) + .[0] | group_by(.date) | map(add)) | map(select((.date < "2020-09-03" or .date > "2020-09-09") and .date != "2021-03-15" and .date != "2021-05-10" and .date != "2021-03-14" and .date != "2021-05-09") | [.date, .newCases, .active, .recovered, .currentlyHospitalized, .totalHospitalized, .deaths, .totalConfirmedCases])) | .[1]' casesbyday.json commons.json | expand -t4
+jq -s --tab 'def eval_repeats(key): foreach .[] as $row (0; ($row[key] // .); . as $x | $row | (.[key] = (.[key] // $x))); .[0] as $commons | (.[0].data | map([["date", "newCases", "active", "recovered", "hospitalized", "totalHospitalized", "deaths", "cases"], .] | transpose | map({key: .[0], value: .[1]}) | from_entries)) + .[1] + .[2] + [.[3]] | group_by(.date) | map(add) | [eval_repeats("deaths")] | map([.date, .newCases, .active, (.cases - (.active // 0) - (.deaths // 0))? // null, .hospitalized, .totalHospitalized, .deaths, .cases]) as $data | $commons | .data = $data' commons.json casesbyday.json hosp.json today.json | expand -t4
