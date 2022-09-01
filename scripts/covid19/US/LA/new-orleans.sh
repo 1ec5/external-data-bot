030f7a940c7bb837aacab65fb13c6e37d9cfe04c
Minh Nguyễn <mxn@1ec5.org>
2022-08-31 23:21:22 -0700

    Sort New Orleans by date

--- scripts/covid19/US/LA/new-orleans.sh
+++ scripts/covid19/US/LA/new-orleans.sh
@@ -4,7 +4,7 @@
 curl 'https://commons.wikimedia.org/wiki/Data:COVID-19_cases_in_New_Orleans.tab?action=raw' > commons.json
 
 # Query the FeatureServer table for case counts by day
-curl 'https://gis.nola.gov/arcgis/rest/services/apps/LDH_Data/MapServer/0/query?where=1%3D1&outFields=Date%2CNO_Cases%2CNO_Deaths&returnGeometry=false&f=json' > table.json
+curl 'https://gis.nola.gov/arcgis/rest/services/apps/LDH_Data/MapServer/0/query?where=1%3D1&outFields=Date%2CNO_Cases%2CNO_Deaths&returnGeometry=false&orderByFields=Date&f=json' > table.json
 
 # Convert date from number of milliseconds to YYYY-MM-DD
 jq '.features | map(.attributes | .Date = (.Date / 1000 | strftime("%Y-%m-%d")))' table.json > cases.json
