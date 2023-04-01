3763efc8d1165b9fd3f936b2e047cba3c292f6c1
Minh Nguyễn <mxn@1ec5.org>
2023-03-31 22:46:04 -0700

    Paginated Solano Co. ArcGIS queries

--- scripts/covid19/US/CA/solano-county.sh
+++ scripts/covid19/US/CA/solano-county.sh
@@ -5,17 +5,19 @@ curl 'https://commons.wikimedia.org/wiki/Data:COVID-19_cases_in_Solano_County,_C
 
 # Query the FeatureServer epidemiological curve table
 curl 'https://services2.arcgis.com/SCn6czzcqKAFwdGU/ArcGIS/rest/services/Epi_Curve/FeatureServer/0/query?where=1%3D1&outFields=date_collected%2Cdaily_number%2Ccumulative_running_total_number&orderByFields=date_collected&f=json' > table.json
+curl 'https://services2.arcgis.com/SCn6czzcqKAFwdGU/ArcGIS/rest/services/Epi_Curve/FeatureServer/0/query?where=1%3D1&outFields=date_collected%2Cdaily_number%2Ccumulative_running_total_number&orderByFields=date_collected&resultOffset=1000&f=json' > table2.json
 
 # Convert date from number of milliseconds to YYYY-MM-DD
 # Accumulate cases
 # Calculate recoveries by subtracting deaths and active cases from confirmed cases
-jq '.features | map(.attributes | {date: (.date_collected / 1000 | gmtime | .[3] -= 7 | mktime | strftime("%Y-%m-%d")), newCases: .daily_number, active: .cumulative_running_total_number}) | [foreach .[] as $date (0; . + $date.newCases; . as $cases | $date | .cases = $cases)]' table.json > casesbyday.json
+jq -s '.[0].features + .[1].features | map(.attributes | {date: (.date_collected / 1000 | gmtime | .[3] -= 7 | mktime | strftime("%Y-%m-%d")), newCases: .daily_number, active: .cumulative_running_total_number}) | [foreach .[] as $date (0; . + $date.newCases; . as $cases | $date | .cases = $cases)]' table.json table2.json > casesbyday.json
 
 # Query the FeatureServer hospitalization table
 curl 'https://services2.arcgis.com/SCn6czzcqKAFwdGU/arcgis/rest/services/Hospital_Stats/FeatureServer/0/query?where=number_inpatient_by_day+IS+NOT+NULL&outFields=date_%2Cnumber_inpatient_by_day&orderByFields=date_&f=json' > table.json
+curl 'https://services2.arcgis.com/SCn6czzcqKAFwdGU/arcgis/rest/services/Hospital_Stats/FeatureServer/0/query?where=number_inpatient_by_day+IS+NOT+NULL&outFields=date_%2Cnumber_inpatient_by_day&orderByFields=date_&resultOffset=1000&f=json' > table2.json
 
 # Convert date from number of milliseconds to YYYY-MM-DD
-jq '.features | map(.attributes | {date: (.date_ / 1000 | gmtime | .[3] -= 7 | mktime | strftime("%Y-%m-%d")), hospitalized: .number_inpatient_by_day})' table.json > hosp.json
+jq -s '.[0].features + .[1].features | map(.attributes | {date: (.date_ / 1000 | gmtime | .[3] -= 7 | mktime | strftime("%Y-%m-%d")), hospitalized: .number_inpatient_by_day})' table.json table2.json > hosp.json
 
 # Query the FeatureServer vaccination table and its metadata
 curl 'https://services2.arcgis.com/SCn6czzcqKAFwdGU/ArcGIS/rest/services/Vaccine_Summary/FeatureServer/0/query?where=1%3D1&outFields=*&f=json' > table.json
