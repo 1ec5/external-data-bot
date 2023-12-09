f0be1b27ac06156574317892176bf5107caf0f6f
Minh Nguyễn <mxn@1ec5.org>
2023-12-09 02:48:48 -0800

    Switched to current Sonoma Co. deaths table

--- scripts/covid19/US/CA/sonoma-county.sh
+++ scripts/covid19/US/CA/sonoma-county.sh
@@ -5,7 +5,13 @@ curl 'https://commons.wikimedia.org/wiki/Data:COVID-19_cases_in_Sonoma_County,_C
 
 # Query the FeatureServer table for cases by day
 curl 'https://services1.arcgis.com/P5Mv5GY5S66M8Z1Q/ArcGIS/rest/services/NCOV_Cases_Sonoma_County/FeatureServer/0/query?where=Date+<>+null&outFields=Date%2CNewCases%2CActive%2CDeaths%2CRecovered%2CCumulative&orderByFields=Date&f=json' > table.json
+jq 'def diff(key): [[{(key): 0}] + .[:-1], .] | transpose | map(.[1]["diff:" + key] = (if .[1][key] and .[0][key] then (.[1][key] - .[0][key]) else null end) | .[1]); [.features[].attributes | .date = (.Date / 1000 | gmtime | .[3] -= 7 | mktime | strftime("%Y-%m-%d"))] | sort_by(.date)' table.json > cases.json
+
+# Query the FeatureServer for deaths by day
+# Calculate running total of deaths
+curl 'https://services1.arcgis.com/P5Mv5GY5S66M8Z1Q/arcgis/rest/services/NCOV_CaseData_DeathsTimeline/FeatureServer/0/query?f=json&resultRecordCount=32000&where=DateofDeath%20IS%20NOT%20NULL&orderByFields=DateofDeath%20asc&outFields=DateofDeath,Deaths&resultType=standard&returnGeometry=false' > table.json
+jq 'def total(key): foreach .[] as $row (0; . + $row[key]; . as $x | $row | (.["total:" + key] = $x)); .features | map(.attributes) | group_by(.DateofDeath) | map(add) | [total("Deaths")] | map(.date = (.DateofDeath / 1000 | gmtime | .[3] -= 7 | mktime | strftime("%Y-%m-%d")) | {date: .date, deaths: .["total:Deaths"]})' table.json > deaths.json
 
 # Convert date from number of milliseconds to YYYY-MM-DD
 # Update Commons
-jq -s --tab 'def diff(key): [[{(key): 0}] + .[:-1], .] | transpose | map(.[1]["diff:" + key] = (if .[1][key] and .[0][key] then (.[1][key] - .[0][key]) else null end) | .[1]); .[1].data = ([.[0].features[].attributes | (.Date / 1000 | gmtime | .[3] -= 7 | mktime | strftime("%Y-%m-%d")) as $date | [$date, .NewCases, .Active, .Deaths, .Recovered, .Cumulative]] | sort_by(.[0]) | map({cumulative: .[5], row: .}) | diff("cumulative") | map(.row[1] = (.row[1] // .["diff:cumulative"]) | .row)) | .[1]' table.json commons.json | expand -t4
+jq -s --tab 'def eval_repeats(key): foreach .[] as $row (0; ($row[key] // .); . as $x | $row | (.[key] = (.[key] // $x))); def diff(key): [[{(key): 0}] + .[:-1], .] | transpose | map(.[1]["diff:" + key] = (if .[1][key] and .[0][key] then (.[1][key] - .[0][key]) else null end) | .[1]); .[0].data = ((.[1] + .[2] | group_by(.date) | map(add)) | [eval_repeats("deaths")] | map([.date, .NewCases, .Active, .deaths, .Recovered, .Cumulative]) | map({cumulative: .[5], row: .}) | diff("cumulative") | map(.row[1] = (.row[1] // .["diff:cumulative"]) | .row)) | .[0]' commons.json cases.json deaths.json | expand -t4
