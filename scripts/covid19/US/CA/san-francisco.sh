4fb67f6b131bbd449ab344440e3fc0a1223f4c00
Minh Nguyễn <mxn@1ec5.org>
2025-10-08 23:04:03 -0700

    Increased San Francisco record limit

--- scripts/covid19/US/CA/san-francisco.sh
+++ scripts/covid19/US/CA/san-francisco.sh
@@ -7,7 +7,7 @@ curl 'https://commons.wikimedia.org/wiki/Data:COVID-19_cases_in_San_Francisco.ta
 curl 'https://data.sfgov.org/resource/d2ef-idww.json?$select=specimen_collection_date%20as%20date,new_confirmed_cases%20as%20newConfirmedCases,cumulative_confirmed_cases%20as%20totalConfirmedCases&area_type=Citywide&$limit=2000' | jq 'map(.date = (.date | split("T")[0]))' > cases.json
 
 # Fetch the deaths by day from the API
-curl 'https://data.sfgov.org/resource/g2di-xufg.json?$select=date_of_death%20as%20date,new_deaths%20as%20newDeaths,cumulative_deaths%20as%20totalDeaths&$limit=2000' | jq 'map(.date = (.date | split("T")[0]))' > deaths.json
+curl 'https://data.sfgov.org/resource/g2di-xufg.json?$select=date_of_death%20as%20date,new_deaths%20as%20newDeaths,cumulative_deaths%20as%20totalDeaths&$limit=3000' | jq 'map(.date = (.date | split("T")[0]))' > deaths.json
 
 # Fetch hospitalizations by day from the API
 curl 'https://data.sfgov.org/resource/nxjg-bhem.json?$select=reportdate%20as%20date,sum%28patientcount%29%20as%20hospitalized&$group=reportdate&$order=reportdate&$limit=2000' | jq 'map(.date = (.date | split("T")[0]))' > hosp.json
